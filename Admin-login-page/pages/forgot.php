4384ff295a20af381ef36faaf3dffebd90815759
Karthik_GV <85121069+GVKarthik-dev@users.noreply.github.com>
2023-09-05 17:46:19 +0530

    Update forgot.php

--- Admin-login-page/pages/forgot.php
+++ Admin-login-page/pages/forgot.php
@@ -2,9 +2,6 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Process the password recovery request here
-    // Send a password reset email or perform the necessary actions
-    // Redirect the user to a confirmation page or the login page
     header("Location: recovery_confirmation.php");
     exit();
 }
@@ -13,14 +10,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../css/styles.css"> <!-- Include your CSS file -->
+    <link rel="stylesheet" href="../css/styles.css"> 
 </head>
 <body>
     <div class="container">
         <div class="form-box">
             <h2>Forgot Password</h2>
             <form action="forgot_password.php" method="POST">
-                <!-- Your form fields here -->
                 <button type="submit" class="btn btn-primary">Reset Password</button>
             </form>
         </div>
