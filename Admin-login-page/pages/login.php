c2b02db96a75daa974be84476b62dce9d3d2addf
Karthik_GV <85121069+GVKarthik-dev@users.noreply.github.com>
2023-09-05 17:48:34 +0530

    Update login.php

--- Admin-login-page/pages/login.php
+++ Admin-login-page/pages/login.php
@@ -2,18 +2,15 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Perform user authentication (you should replace this with your own authentication logic)
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Replace this with your actual authentication logic (e.g., database queries)
     $validUsername = "user123";
     $validPassword = "password123";
 
     if ($username === $validUsername && $password === $validPassword) {
-        // Authentication successful
         $_SESSION["username"] = $username;
-        header("Location: dashboard.php"); // Redirect to the dashboard or another authenticated page
+        header("Location: dashboard.php"); 
         exit();
     } else {
         $loginError = "Wrong username or password. Please try again.";
@@ -24,14 +21,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../css/styles.css"> <!-- Include your CSS file -->
+    <link rel="stylesheet" href="../css/styles.css"> 
 </head>
 <body>
     <div class="container">
         <div class="form-box">
             <h2>Login</h2>
             <form action="login.php" method="POST">
-                <!-- Your form fields here -->
                 <button type="submit" class="btn btn-primary">Login</button>
                 <?php
                 if (isset($loginError)) {
